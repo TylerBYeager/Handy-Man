987ed8dd473af0fd589787a32ae57c1940e0c011
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-03 10:50:35 -0700

    add create user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,10 +2,17 @@ const router = require('express').Router();
 const {User, Pending} = require('../../models');
 
 // User Sign Up
-router.post('/signup', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-      
+      const newUser = await User.create(req.body);
+
+      req.session.save(() => {
+        req.session.user_id = newUser.isSoftDeleted;
+        req.session.loggedIn = true;
+        res.json(newUser);
+      });
     }catch(err){
+      console.log(err);
       res.status(500).json(err)
     }
 });
