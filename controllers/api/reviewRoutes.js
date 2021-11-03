c629cdf088ae3e763e945b204082cead49d6a528
TZ <tz116917@gmail.com>
2021-11-03 11:17:04 -0700

    category routes complete

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -4,16 +4,21 @@ const {Review, Pending} = require('../../models');
 // View review by pendingjob id
 router.get('/:id', async (req, res) => {
     try {
-      
+      const Data = await Pending.findByPk(req.params.id, {
+        include: [{model:Review}]
+      });
+      res.status(200).json(Data)
     }catch(err){
       res.status(500).json(err)
     }
 });
 
 // Post review for pendingjob by id
-router.post('/:id', async (req, res) => {
+// {review_text: , user_id: , vendor_id: , pending_id: }
+router.post('/', async (req, res) => {
     try {
-      
+      const Data = await Review.create(req.body);
+      res.status(200).json(Data)
     }catch(err){
       res.status(500).json(err)
     }
