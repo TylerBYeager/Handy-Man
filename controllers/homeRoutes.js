a9dbabaf4622720293af56ece8bccf418a65befd
isa <5445946+icortes@users.noreply.github.com>
2021-11-02 20:02:30 -0700

    made routes for nav bar buttons

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,34 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    try {
-      res.render('homepage');
-    }catch(err){
-      res.status(500).json(err);
-    }
+router.get("/", (req, res) => {
+  try {
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+router.get("/search", (req, res) => {
+  try {
+    res.render("search");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  try {
+    res.render("login-choice");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/contact-us", (req, res) => {
+  try {
+    res.render("contact-us");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
