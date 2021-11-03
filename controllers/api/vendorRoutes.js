b2fb053728c636ae87468618de3e14d193417bbc
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-03 11:22:05 -0700

    add vendor sign in route

--- controllers/api/vendorRoutes.js
+++ controllers/api/vendorRoutes.js
@@ -1,68 +1,73 @@
 const router = require('express').Router();
-const {Vendor, Pending, Category} = require('../../models');
+const { Vendor, Pending, Category } = require('../../models');
 
 // Vendor Sign Up
-router.post('/signup', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
-    }
+router.post('/', async (req, res) => {
+  try {
+    const newVendor = await Vendor.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = newVendor.id;
+      req.session.loggedIn = true;
+      res.json(newVendor);
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // Vendor Log In
 router.post('/login', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
-    }
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // Vendor Log Out
 router.post('/logout', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
-    }
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // View vendor by ID
 router.get('/:id', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
-    }
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // View pending jobs by Vendor
 router.get('/pending', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
-    }
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // View pending jobs by ID
 router.get('/pending/:id', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
-    }
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // Approve job request
 router.put('/approve/:id', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
-    }
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
-  
