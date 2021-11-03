e3688cd25fc8ccca659d5c1f41173e0edd828676
TZ <tz116917@gmail.com>
2021-11-03 12:00:08 -0700

    user add new request route complete

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -29,9 +29,11 @@ router.post('/logout', async (req, res) => {
 });
 
 // User submit new request
+// {name: , description: , address: , vendor_id: , user_id:, category_id: }
 router.post('/new-request', async (req, res) => {
     try {
-      
+      const Data = await Pending.create(req.body);
+      res.status(200).json(Data)
     }catch(err){
       res.status(500).json(err)
     }
