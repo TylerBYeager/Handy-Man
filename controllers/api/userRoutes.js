62cb9b9fc5ce3feadd8a907e0a09f719c7896e52
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-03 11:15:11 -0700

    add user update and delete routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,64 +1,107 @@
 const router = require('express').Router();
-const {User, Pending} = require('../../models');
+const { User, Pending } = require('../../models');
+const { update } = require('../../models/Category');
 
 // User Sign Up
 router.post('/', async (req, res) => {
-    try {
-      const newUser = await User.create(req.body);
+  try {
+    const newUser = await User.create(req.body);
 
-      req.session.save(() => {
-        req.session.user_id = newUser.isSoftDeleted;
-        req.session.loggedIn = true;
-        res.json(newUser);
-      });
-    }catch(err){
-      console.log(err);
-      res.status(500).json(err)
-    }
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.loggedIn = true;
+      res.json(newUser);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err)
+  }
 });
 
 // User Log In
 router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
-      if (!userData) {
-        res.status(400).json({ message: "Incorrect email or password. Try again"});
-        return;
-      }
-
-      const validPassword = await userData.checkPassword(req.body.password);
-      if (!validPassword) {
-        res.status(400).json({ message: "Incorrect email or password. Try again"});
-        return;
-      }
-
-      req.session.save(() => {
-        req.session.user_id = userData.isSoftDeleted;
-        req.session.loggedIn = true;
-
-        res.json({ user: userData, message: "Successfully logged in!"});
-      });
-    }catch(err){
-      res.status(500).json(err)
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res.status(400).json({ message: "Incorrect email or password. Try again" });
+      return;
     }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect email or password. Try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.isSoftDeleted;
+      req.session.loggedIn = true;
+
+      res.json({ user: userData, message: "Successfully logged in!" });
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // User Log Out
 router.post('/logout', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updateUser = await User.update(req.body,
+      {
+        individualHooks: true,
+        where: {
+          id: req.params.id
+        }
+      });
+    if (!updateUser) {
+      res.status(404).json({ message: "No user found" });
+      return;
+    } else {
+      res.status(200).json(updateUser);
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deleteUser = await User.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteUser) {
+      res.status(404).json({ message: "User does not exist" });
+      return
+    } else {
+      res.status(200).json(deleteUser);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // User submit new request
 router.post('/new-request', async (req, res) => {
-    try {
-      
-    }catch(err){
-      res.status(500).json(err)
-    }
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
