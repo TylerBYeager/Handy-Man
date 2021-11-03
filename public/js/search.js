dd0309467d38bdd73e97e0b9529b66cce01fd401
TZ <tz116917@gmail.com>
2021-11-03 16:30:26 -0700

    search button now displays vendors

--- public/js/search.js
+++ public/js/search.js
@@ -28,28 +28,40 @@ loadCategory()
 function display(event){
     event.preventDefault();
     console.log(Searchbar.value)
+    if(Searchbar.value!="Select a category"){
+        fetch(`/api/categories/vendor/${Searchbar.value}`)
+        .then(function(result){
+            return result.json()
+        })
+        .then(function(result) {
+            console.log(result)
 
-    fetch(`/api/categories/vendor/${Searchbar.value}`)
-    .then(function(result){
-        return result.json()
-    })
-    .then(function(result) {
-        console.log(result)
+            for(i=0; i<result.vendors.length; i++){
+                let newCard = document.createElement("div")
+                newCard.setAttribute("class", "card vendor-card")
 
-        for(i=0; i<result.vendors.length; i++){
-            let newCard = document.createElement("div")
-            newCard.setAttribute("class", "card")
-            let newCardTitle = document.createElement("h4")
-            newCardTitle.setAttribute("class", "card-title")
-            let newCardtext = document.createElement("p")
-            newCardtext.setAttribute("class", "card-text")
-            let newCardlink = document.createElement("a")
-            newCardlink.setAttribute("class", "btn btn-primary")
-            newCardlink.innerText = "Visit Profile"
-            newCard.appendChild(newCardTitle)
-            newCard.appendChild(newCardtext)
-            newCard.appendChild(newCardlink)
-        }
-    })
+                let newCardTitle = document.createElement("h4")
+                newCardTitle.setAttribute("class", "card-title")
+                newCardTitle.innerText = result.vendors[i].business_name
+                newCard.appendChild(newCardTitle)
+
+                let newCardtext = document.createElement("p")
+                newCardtext.setAttribute("class", "card-text")
+                newCardtext.innerText = `Owned by: ${result.vendors[i].name}`
+                newCard.appendChild(newCardtext)
+
+
+                let newCardlink = document.createElement("a")
+                newCardlink.setAttribute("class", "btn btn-primary")
+                newCardlink.setAttribute("href", "vendor profile page")
+                newCardlink.innerText = "Visit Profile"
+                newCard.appendChild(newCardlink)
+
+                DisplayArea.appendChild(newCard)
+                
+            }
+        })
+    }
 }
 
+document.querySelector("#search-category").addEventListener("click", display)
