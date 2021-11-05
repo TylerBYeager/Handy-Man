c108bf6690fe2093f33e9c9588df4debcaa7b052
TZ <tz116917@gmail.com>
2021-11-05 00:24:24 -0700

    approval button works

---
+++ public/js/vendor-dashboard.js
@@ -0,0 +1,21 @@
+const approveArr = document.querySelectorAll('.approve')
+console.log(approveArr)
+
+async function approve(id){
+    const approve_res = await fetch(`/api/vendors/approve/${id}`, {
+        method: "PUT"
+    })
+
+    if(approve_res.ok) {
+        window.location.reload(true);
+    }
+}
+
+for(i=0;i<approveArr.length;i++){
+    let pending_id = approveArr[i].id.split('_')[1]
+    console.log("pending_id is: "+pending_id)
+    approveArr[i].addEventListener('click', function(event){
+        event.preventDefault
+        approve(pending_id)
+    })
+}
