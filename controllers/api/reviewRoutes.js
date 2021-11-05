4d0746191ef0b9339c91bd10d3e3b3eb9610c03a
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-04 20:21:20 -0700

    add readme content, modify Data to data variable, add gif

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -5,13 +5,13 @@ const withAuth = require("../../utils/auth");
 // View review by pendingjob id
 router.get('/:id', async (req, res) => {
     try {
-      const Data = await Pending.findByPk(req.params.id, {
+      const data = await Pending.findByPk(req.params.id, {
         include: [{model:Review}]
       });
-      if (!Data) {
+      if (!data) {
         res.status(404).json("Pending job doesn't exist");
       } else {
-        res.status(200).json(Data)
+        res.status(200).json(data)
       }
     }catch(err){
       res.status(500).json(err)
@@ -22,8 +22,8 @@ router.get('/:id', async (req, res) => {
 // {review_text: , user_id: , vendor_id: , pending_id: }
 router.post('/', withAuth, async (req, res) => {
     try {
-      const Data = await Review.create(req.body);
-      res.status(200).json(Data)
+      const data = await Review.create(req.body);
+      res.status(200).json(data)
     }catch(err){
       res.status(500).json(err)
     }
