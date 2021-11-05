d55cea0b733943e5765477550663f68275f1a734
isa <5445946+icortes@users.noreply.github.com>
2021-11-04 19:32:40 -0700

    comment of how the scroll bar works

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+/**
+ * DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, 
+ * without waiting for stylesheets, images, and subframes to finish loading.
+ **/   
+document.addEventListener("DOMContentLoaded", function () {
 	//The first argument are the elements to which the plugin shall be initialized
 	//The second argument has to be at least a empty object or a object with your desired options
 	OverlayScrollbars(document.querySelectorAll("body"), { });
